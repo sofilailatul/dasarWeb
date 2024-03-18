6e6b1a52057306b8ecaf046a541832400f22c448
sofilailatul <lailatulsofi11@gmail.com>
2024-03-18 10:18:09 +0700

    Pertemuan 5 - Praktikum 8 Date & Time

---
+++ Pertemuan5/date.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3> Date </h3>
+    <?php
+        echo "Today is " . date("Y/m/d") . "<br>";
+        echo "Today is " . date("Y.m.d") . "<br>";
+        echo "Today is " . date("Y-m-d") . "<br>";
+        echo "Today is " . date("l");
+    ?>
+</body>
+</html>
