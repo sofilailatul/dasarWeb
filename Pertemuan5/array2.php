7f71a7c14fe175d483d7c40c6a17775483976582
sofilailatul <lailatulsofi11@gmail.com>
2024-03-18 09:12:36 +0700

    Pertemuan 5 - Praktikum 2

---
+++ Pertemuan5/array2.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Dosen</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: 20px auto;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'domisili' => 'Malang',
+            'jenis_kelamin' => 'Perempuan'
+        ];
+    ?>
+
+    <table>
+        <tr>
+            <th>Informasi Dosen</th>
+            <th>Detail</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
