920d8e317483e8c1b9b0a1faa30d38e5be35f017
sofilailatul <lailatulsofi11@gmail.com>
2024-03-18 09:05:45 +0700

    Pertemuan 5 - Praktikum 1

---
+++ Pertemuan5/array1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+    // echo $Listdosen[2] . "<br>";
+    // echo $Listdosen[0] . "<br>";
+    // echo $Listdosen[1] . "<br>";
+
+    for($i = 0; $i < count($Listdosen); $i++){
+        echo $Listdosen[$i] . "<br>";
+    }
+    ?>
+</body>
+</html>
