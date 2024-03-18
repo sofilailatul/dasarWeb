d97ecefff9e92010d39230ad0a6eb1d78a237c53
sofilailatul <lailatulsofi11@gmail.com>
2024-03-18 10:09:49 +0700

    Pertemuan 5 - Praktikum 6 Merge PHP & HTML

---
+++ Pertemuan5/mergePhpHtml2.php
@@ -0,0 +1,8 @@
+<?php
+echo '<html>';
+echo '<head><title>Cara 02</title></head>';
+echo '<body>'; // Tambahkan titik koma di sini
+echo '<p>Tanggal Hari ini : '. date('d M Y').'</p>';
+echo '</body>';
+echo '</html>';
+?>
