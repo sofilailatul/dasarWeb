f7f96fb75fc8d6ada5dbba2b392db2da3cbb29b1
sofilailatul <lailatulsofi11@gmail.com>
2024-03-29 21:26:51 +0700

    Pertemuan 8 - Praktikum 3

---
+++ Pertemuan8/form_upload_ajax.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Unggah File Dokumen</title>
+        <link rel="stylesheet" href="upload.css" type="text/css">
+    </head>
+    <body>
+        <div class="upload-form-container">
+            <h2>Unggah File Dokumen</h2>
+            <form action="upload_ajax.php" id="upload-form" method="post" enctype="multipart/form-data">
+                <div class="file-input-container">
+                    <input type="file" name="file" id="file" class="file-input">
+                    <label for="file" class="file-label">Pilih File</label>
+                </div>
+                <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+            </form>
+            <div id="status" class="upload-status"></div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="upload.js"></script>
+    </body>
+</html>
